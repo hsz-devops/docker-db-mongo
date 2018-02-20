0a8a6a27369e3afe26c108a79646d3d6bb396849
Joao C Costa <joaocc-dev@live.com>
2018-02-20 11:19:03 +0000

    upgrading v3.6 (untested)

--- 3.6/src/docker-entrypoint.keygen.sh
+++ 3.6/src/docker-entrypoint.keygen.sh
@@ -23,6 +23,38 @@ cmd="$mongodb_cmd "
 [ "${MGO__HTTP_ENABLED}" == "yes" ] &&  cmd="$cmd --httpinterface"
 [ "${MGO__REST_ENABLED}" == "yes" ] &&  cmd="$cmd --rest"
 
+# ----------------------------------------------------------------
+if [ "${MGO__REPLICATION_ENABLED}" == "yes" ]; then
+    if [ -z "${MGO__REPLSET_NAME}" ]; then
+        echo "Error: Missing replica set name!"
+        exit -2
+    fi
+
+    cmd="$cmd --replSet $MGO__REPLSET_NAME"
+
+    if [ "${MGO__CLUSTER_INTERNAL_AUTH_ENABLED}" == "yes" ]; then
+        if [ "${MGO__CLUSTER_INTERNAL_AUTH_MODE}" == "keyFile" ]; then
+            if [ -z "${MGO__CLUSTER_INTERNAL_AUTH_KEYFILE}" ]; then
+                echo "Error: Missing cluster internal auth keyfile path!"
+                exit -2
+            fi
+            if [ ! -f "${MGO__CLUSTER_INTERNAL_AUTH_KEYFILE}" ]; then
+                echo "Error: Cluster internal auth keyfile does not exist!"
+                exit -3
+            fi
+
+            chmod 0400            "${MGO__CLUSTER_INTERNAL_AUTH_KEYFILE}"
+            chown mongodb:mongodb "${MGO__CLUSTER_INTERNAL_AUTH_KEYFILE}"
+
+            cmd="$cmd --clusterAuthMode $MGO__CLUSTER_INTERNAL_AUTH_MODE --keyFile $MGO__CLUSTER_INTERNAL_AUTH_KEYFILE"
+        else
+            echo "Error: Only keyFile internal auth mode supported for now!"
+            exit -4
+        fi
+    fi
+fi
+
+# ----------------------------------------------------------------
 cmd="$cmd --storageEngine ${MGO__STORAGE_ENGINE:-wiredTiger}"
 
 # ----------------------------------------------------------------
@@ -35,8 +67,12 @@ else
     cmd="$cmd --sslMode       ${MGO__SSL_MODE}"
     cmd="$cmd --sslPEMKeyFile ${MGO__SSL_KEYDIR}/mongodb-key.pem"
 
-    [ "${MGO__SSL_FORCE_NEW_KEY}" == "yes" ] && KEYGEN_FORCE="--force"
-    /usr/local/bin/gen_self_signed_cert.sh ${KEYGEN_FORCE} "${MGO__SSL_KEYDIR}" "${MGO__SSL_HOSTNAME}"
+    if [ "${MGO__SSL_SELFSIGN_GEN}" == "yes" ]; then
+        if [ "${MGO__SSL_FORCE_NEW_KEY}" == "yes" ]; then
+            KEYGEN_FORCE="--force"
+        fi
+        /usr/local/bin/gen_self_signed_cert.sh ${KEYGEN_FORCE} "${MGO__SSL_KEYDIR}" "${MGO__SSL_HOSTNAME}"
+    fi
 fi
 
 case "$1" in
